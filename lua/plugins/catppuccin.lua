725e02c144eb08ca02875e88d6e5efeabf3dd0b2
nhussein11 <nicolashussein14@gmail.com>
2024-02-26 00:03:16 -0300

    fix: update colorscheme settings

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,19 @@
-return { 
-	"catppuccin/nvim", 
-	name = "catppuccin", 
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true,
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end
 }
 
 
