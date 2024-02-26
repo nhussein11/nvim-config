eaf5a8b656e86b68f8687b2580e8681a9266c760
nhussein11 <nicolashussein14@gmail.com>
2024-02-26 00:03:25 -0300

    fix: update on focus for neo-tree plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,13 +3,20 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
     require('neo-tree').setup({
       window = {
         width=25
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
       }
     })
     vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>',{})
