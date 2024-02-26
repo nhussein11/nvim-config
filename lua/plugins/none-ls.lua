112661c882df001526287f24dfe3a1e8c94dbfe4
nhussein11 <nicolashussein14@gmail.com>
2024-02-26 00:03:38 -0300

    fix: set auto-start true in none-ls plugin

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,6 +4,7 @@ return {
     local null_ls = require("null-ls")
 
     null_ls.setup({
+      autostart = true,
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
